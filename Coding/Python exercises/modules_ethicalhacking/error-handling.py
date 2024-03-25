dc310ba49130f3561b67b3f3b9303ca8110ca81b
FabulousBernt <johnny.vigersten@icloud.com>
2024-03-25 11:26:18 +0100

    Updated error-handling.py file

--- Coding/Python exercises/modules_ethicalhacking/error-handling.py
+++ Coding/Python exercises/modules_ethicalhacking/error-handling.py
@@ -7,11 +7,15 @@ while True:
         10/age
     except ValueError:
         print("Age must be a number.")
+        continue
     except ZeroDivisionError:
         print("Age must be higher than 0.")
+        continue
     else:
         print("Great age")
         break
+    finally:
+        print("Ok, I'm finally done.")
 
 # # Error Handling 2
 # def sum(num1, num2):
