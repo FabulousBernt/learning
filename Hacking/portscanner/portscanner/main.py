01cd21946cdaa46e5cf3f31356f8592f875953fa
FabulousBernt <johnny.vigersten@icloud.com>
2024-04-05 08:24:28 +0200

    portscanner coding exercise update

--- Hacking/portscanner/portscanner/main.py
+++ Hacking/portscanner/portscanner/main.py
@@ -1,23 +1,27 @@
 import socket
 import termcolor
 
+
 def scan(target, ports):
+    print('\n' + ' Starting Scan For ' + str(target))
     for port in range(1, ports):
-        scan_port(target, ports)
+        scan_port(target, port)
+
 
 def scan_port(ipaddress, port):
     try:
         sock = socket.socket()
         sock.connect((ipaddress, port))
-        print('[+] Port Opened ' + str(port))
+        print("[+] Port Opened " + str(port))
         sock.close()
     except:
-        print('[-] Port Closed ' + str(port))
+        pass
 
-targets = int(input('[*] Enter target to scan (Split them by ,): '))
-ports = input('[*] Enter how many ports you want to scan: ')
+
+targets = input("[*] Enter Targets To Scan(split them by ,): ")
+ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
 if ',' in targets:
-    print('[*] Scanning multiple targets')
+    print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
     for ip_addr in targets.split(','):
         scan(ip_addr.strip(' '), ports)
 else:
